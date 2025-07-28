4d1bdfdda693879fe2c8c86770a99c981ceb943a
sirhCC <ccharles199220@gmail.com>
2025-07-28 13:55:40 -0400

    feat: Add initial CI/CD provider module exports and related types

---
+++ dist/providers/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Provider Module Exports
+ * Centralized exports for all CI/CD provider related functionality
+ */
+export { BaseCICDProvider } from './base.provider';
+export { GitHubActionsProvider } from './github-actions.provider';
+export type { ProviderConfig, PipelineData, JobData, StepData, RunnerData, ResourceUsageData, ArtifactData, LogData, WebhookPayload, ProviderMetrics, } from './base.provider';
+export { ProviderFactory, providerFactory } from './factory';
+export type { ProviderRegistration } from './factory';
+export { PipelineProvider } from '../types';
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/providers/index.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * Provider Module Exports
+ * Centralized exports for all CI/CD provider related functionality
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PipelineProvider = exports.providerFactory = exports.ProviderFactory = exports.GitHubActionsProvider = exports.BaseCICDProvider = void 0;
+// Core provider abstractions
+var base_provider_1 = require("./base.provider");
+Object.defineProperty(exports, "BaseCICDProvider", { enumerable: true, get: function () { return base_provider_1.BaseCICDProvider; } });
+var github_actions_provider_1 = require("./github-actions.provider");
+Object.defineProperty(exports, "GitHubActionsProvider", { enumerable: true, get: function () { return github_actions_provider_1.GitHubActionsProvider; } });
+// Provider factory and registry
+var factory_1 = require("./factory");
+Object.defineProperty(exports, "ProviderFactory", { enumerable: true, get: function () { return factory_1.ProviderFactory; } });
+Object.defineProperty(exports, "providerFactory", { enumerable: true, get: function () { return factory_1.providerFactory; } });
+// Re-export provider-related types
+var types_1 = require("../types");
+Object.defineProperty(exports, "PipelineProvider", { enumerable: true, get: function () { return types_1.PipelineProvider; } });
+//# sourceMappingURL=index.js.map
