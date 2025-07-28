a5d311e716674c0bc4ecef277f5498c7b9fbd823
sirhCC <ccharles199220@gmail.com>
2025-07-28 02:11:54 -0400

    Add initial migration schema with source map

---
+++ dist/migrations/1701234567890-InitialSchema.d.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+export declare class InitialSchema1701234567890 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
+//# sourceMappingURL=1701234567890-InitialSchema.d.ts.map
